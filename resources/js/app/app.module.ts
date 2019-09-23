2425a475375f2933e625e819e2462d7893fb2250
Mareks Dāvis Riška <mareks.riska@autentica.lv>
2019-09-23 18:12:04 +0300

    Angular base..

--- resources/js/app/app.module.ts
+++ resources/js/app/app.module.ts
@@ -1,20 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgProgressModule} from "@ngx-progressbar/core";
+import {NgProgressHttpModule} from "@ngx-progressbar/http";
+import {NgProgressRouterModule} from "@ngx-progressbar/router";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NgProgressModule.withConfig({
+            // trickleSpeed: 200,
+            // min: 20,
+            // meteor: false
+            // https://github.com/MurhafSousli/ngx-progressbar/wiki/Global-config
+        }),
+        NgProgressHttpModule,
+        NgProgressRouterModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
